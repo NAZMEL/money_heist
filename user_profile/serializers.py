90d5d2c791a819b38a65335037d13dc2e5b89210
NAZMEL <melnyknasar@gmail.com>
2021-08-21 21:42:40 +0300

    Add Get Users data endpoint

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -3,10 +3,19 @@ from authentication.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    data_joined = serializers.ReadOnlyField()
 
     class Meta:
         model = User
-        fields = ('id', "email", "is_active", "password")
-        read_only_fields = ("id", "is_active")
-        write_only_fields = ("password")
+        fields = ('id', 'email', 'is_active', 'password')
+        read_only_fields = ('id',)
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
