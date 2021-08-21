8dcc6508866aa6054a9558e3d34c3a87e4487bb5
NAZMEL <melnyknasar@gmail.com>
2021-08-21 22:14:43 +0300

    Add get users info end-point

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from user_profile.views import ProfileViewSet
+from user_profile.views import *
 
 app_name = 'user_profile'
 
 urlpatterns = [
-    path("", ProfileViewSet.as_view({"get": 'retrieve'})),
-    path("change_email/", ProfileViewSet.as_view({"patch": 'partial_update'})),
-    path("change_password/", ProfileViewSet.as_view({"patch": 'partial_update'})),
+    path('', ProfileViewSet.as_view({'get': 'retrieve'})),
+    path('change_email/', ProfileViewSet.as_view({'patch': 'partial_update'})),
+    path('get_profiles_info/', user_profiles_view)
 ]
 
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,42 +1,41 @@
+from rest_framework.generics import UpdateAPIView
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import mixins
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 from authentication.models import User
-
 from user_profile.serializers import UserSerializer
+
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework import permissions
 
 
 class ProfileViewSet(mixins.RetrieveModelMixin,
                      mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
                      viewsets.GenericViewSet):
 
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (AllowAny, )
     queryset = User.objects.all()
-
     serializer_class = UserSerializer
 
     def get_object(self):
         return self.request.user
 
-    # def update(self, request, *args, **kwargs):
-    #     partial = kwargs.pop('partial', False)
-    #     instance = self.get_object()
-    #     serializer = self.get_serializer(instance, data=request.data, partial=partial)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_update(serializer)
-    #
-    #     if getattr(instance, '_prefetched_objects_cache', None):
-    #         # If 'prefetch_related' has been applied to a queryset, we need to
-    #         # forcibly invalidate the prefetch cache on the instance.
-    #         instance._prefetched_objects_cache = {}
-    #
-    #     return self.update(request, *args, **kwargs)
+    def perform_update(self, serializer):
+        if serializer.is_valid():
+            serializer.save()
+            # send_email_confirmation(user=self.request.user, modified=instance)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def partial_update(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # class ProfileUpdateView()
@@ -48,4 +47,20 @@ class ProfileViewSet(mixins.RetrieveModelMixin,
 #     serializer = self.get_serializer(instance, data=request.data, partial=partial)
 #     serializer.is_valid(raise_exception=True)
 #     self.perform_update(serializer)
+
+
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def user_profiles_view(request):
+    '''
+    Get Users info
+    '''
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+
+
 
