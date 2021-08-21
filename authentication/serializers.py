37c48d6f2358b46a05a49c1209a0a7d752b658af
NAZMEL <melnyknasar@gmail.com>
2021-08-21 21:42:13 +0300

    Add Sign-Up function

---
+++ authentication/serializers.py
@@ -0,0 +1,83 @@
+from django.conf import settings
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from rest_framework import serializers
+from authentication.models import User
+from authentication.tokens import TokenGenerator
+from money_heist.tasks import send_email
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password', 'is_active')
+        read_only_fields = ('id', )
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        token = f'{urlsafe_base64_encode(force_bytes(user.email))}.{TokenGenerator.make_token(user)}'
+        url = f'{settings.USER_ACTIVATION_URL}?token={token}'
+        context = {
+            'url': url,
+            'email': user.email
+        }
+
+        template = 'notifications/activate_user.html'
+
+        send_email.delay(
+            subject="Activate your ChooseOne account",
+            template=template,
+            recipients=[user.email],
+            context=context
+        )
+
+        return user
+
+
+class ActivateUserSerializer(serializers.Serializer):
+    token = serializers.CharField()
+    custom_fields = serializers.SerializerMethodField()
+
+    def get_is_sold(self, obj):
+        return True
+
+
+    def validate(self, attrs):
+        token = attrs['token']
+        error_text = f"Provided activation token '{token}' is not valid"
+        try:
+            email, token = token.split('.')
+            email = force_text(urlsafe_base64_decode(email))
+        except (TypeError, ValueError):
+            raise serializers.ValidationError(error_text)
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(error_text)
+
+        if not TokenGenerator.check_token(user, token):
+            raise serializers.ValidationError(error_text)
+
+        attrs['email'] = email
+        return attrs
+
+    def activate_user(self):
+        user = User.objects.get(email=self.validated_data['email'])
+        user.is_active = True
+        user.save()
+        return user
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
--- authentication/urls.py
+++ authentication/urls.py
@@ -4,16 +4,18 @@ from authentication.models import User
 from authentication import views
 
 from django.urls import path, include
-from django.contrib import admin
+from .views import *
 
 
 app_name = 'authentication'
 
 urlpatterns = [
-    # path('', views.ObtainJSONWebToken.as_view(), name='auth'),
     path('', TokenObtainPairView.as_view(), name='obtain'),
     path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('register/', user_register, name='user-register'),
+    path('activate/', ActivateUserView.as_view(), name='user-activate'),
 ]
+
 # path('verify/', views.VerifyJSONWebToken.as_view(), name='auth-verify'),
 # path('refresh/', views.RefreshJSONWebToken.as_view(), name='auth-refresh'),
 # path('sign-up/', views.SignUpView.as_view(), name='sign-up'),
