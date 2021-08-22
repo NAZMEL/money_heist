fde2583cf4470c0b45c1472c682200fe46167da0
zaliubovskiy <77985743+zaliubovskiy@users.noreply.github.com>
2021-08-22 21:49:07 +0300

    Add Change email endpoint

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from user_profile.views import ProfileViewSet, ChangePasswordView
+from user_profile.views import ProfileViewSet, ChangePasswordView, UpdateProfileView
 
 app_name = 'user_profile'
 
 urlpatterns = [
     path('', ProfileViewSet.as_view({'get': 'retrieve'})),
-    path('change_email/', ProfileViewSet.as_view({'patch': 'partial_update'})),
     path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='change_password'),
+    path('change_email/<int:pk>/', UpdateProfileView.as_view(), name='change_email'),
     ]
